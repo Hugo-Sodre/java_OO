ae6a888275e3a3b1a5fc4aaaad9b7c4aa3348c21
Hugo Sodré <hugosodre031@gmail.com>
2023-08-24 00:24:41 -0300

    aula de construtores

---
+++ Constrututores/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter product name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter product price: ");
+        double price = sc.nextDouble();
+        System.out.print("Enter quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantityToAdd = sc.nextInt();
+        product.addProducts(quantityToAdd);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        int quantityToRemove = sc.nextInt();
+        product.removeProducts(quantityToRemove);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        sc.close();
+    }
+}
