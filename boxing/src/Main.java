b30351e6c9872fad0a128743edd6cc9ce149c9f6
Hugo Sodré <hugosodre031@gmail.com>
2023-10-22 23:00:07 -0300

    boxing

---
+++ boxing/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int x = 20;
+
+        Integer obj = x ;
+
+        System.out.println(x);
+
+        int y = obj * 2;
+
+        System.out.println(y);
+    }
+}
