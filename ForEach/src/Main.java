8e22c5cc982933f02c1330806e2c48716a625243
Hugo Sodré <hugosodre031@gmail.com>
2023-10-22 23:04:36 -0300

    forEach

---
+++ ForEach/src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+
+
+        for (int i = 0 ; i< vect.length; i++){
+            System.out.println(vect[i]);
+        }
+
+        System.out.println("-----------------------------------");
+
+        for(String obj : vect){
+            System.out.println(obj);
+        }
+    }
+}
